4727a4a9e15e3c544db2387c52a78243ca80696d
Jiayuan Zhu <u7391461@anu.edu.au>
2022-09-26 19:39:35 +0800

    edit constructor of Inventory

--- src/main/java/game/Inventory.java
+++ src/main/java/game/Inventory.java
@@ -1,38 +1,12 @@
 package game;
 
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-
 public class Inventory {
     private String invName;
     private int amount;
 
-    public Inventory(String itemName) throws IOException, ParseException {
-        JSONParser parser = new JSONParser();
-        Object obj =  parser.parse(new FileReader("./src/Configs/Engine.json"));
-        JSONObject jsonObjectTop = (JSONObject) obj;
-
-        if (itemName == "h1") {
-            this.invName = "smallHealth";
-            this.amount = (int)jsonObjectTop.get("h1");;
-        }
-        else if (itemName == "h2") {
-            this.invName = "bigHealth";
-            this.amount = (int)jsonObjectTop.get("h2");;
-        }
-        else if (itemName == "s1") {
-            this.invName = "smallStamina";
-            this.amount = (int)jsonObjectTop.get("s1");;
-        }
-        else{
-            this.invName = "bigStamina";
-            this.amount = (int)jsonObjectTop.get("s2");;
-        }
+    public Inventory(String invName, int amount) {
+        this.invName = invName;
+        this.amount = amount;
     }
 
     /**
@@ -60,10 +34,11 @@ public class Inventory {
      */
     public String getDes() {
         return switch (this.invName) {
-            case "smallHealth" -> "Small Healing Potion: Add "+ this.amount+ " HP";
-            case "bigHealth" -> "Big Healing Potion: Add "+ this.amount+ " HP";
-            case "smallStamina" -> "Small Stamina Booster: Add "+ this.amount+ " Stamina";
-            case "bigStamina" -> "Big Stamina Booster: Add "+ this.amount+ " Stamina";
+            case "h1" -> "Small Healing Potion: Add "+ this.amount+ " HP";
+            case "h2" -> "Big Healing Potion: Add "+ this.amount+ " HP";
+            case "s1" -> "Small Stamina Booster: Add "+ this.amount+ " Stamina";
+            case "s2" -> "Big Stamina Booster: Add "+ this.amount+ " Stamina";
+            default -> "";
         };
     }
 
