938534f5c908a9252b8bcd424159b5c966f702f3
Jiayuan Zhu <u7391461@anu.edu.au>
2022-09-26 19:11:36 +0800

    Edit Inventory to fit Engine

--- src/main/java/game/Inventory.java
+++ src/main/java/game/Inventory.java
@@ -1,41 +1,70 @@
 package game;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Inventory {
-    private InvType invName;
+    private String invName;
     private int amount;
 
-    public Inventory(InvType itemName) {
-        this.invName = itemName;
-        if (invName == InvType.smallHealth) {
-            this.amount = 5;
+    public Inventory(String itemName) throws IOException, ParseException {
+        JSONParser parser = new JSONParser();
+        Object obj =  parser.parse(new FileReader("./src/Configs/Engine.json"));
+        JSONObject jsonObjectTop = (JSONObject) obj;
+
+        if (itemName == "h1") {
+            this.invName = "smallHealth";
+            this.amount = (int)jsonObjectTop.get("h1");;
         }
-        else if (invName == InvType.bigHealth) {
-            this.amount = 10;
+        else if (itemName == "h2") {
+            this.invName = "bigHealth";
+            this.amount = (int)jsonObjectTop.get("h2");;
         }
-        else if (invName == InvType.smallStamina) {
-            this.amount = 5;
+        else if (itemName == "s1") {
+            this.invName = "smallStamina";
+            this.amount = (int)jsonObjectTop.get("s1");;
         }
         else{
-            this.amount = 10;
+            this.invName = "bigStamina";
+            this.amount = (int)jsonObjectTop.get("s2");;
         }
     }
 
     /**
-     * Get the amount of HP or Stamina
+     * Get the amount of Inventory
      *
-     * @return the amount of HP or Stamina that should be increased
+     * @return the amount
      */
     public int getAmount() {
         return amount;
     }
 
     /**
-     * Get the name of the Inventory
+     * Get tha name of Inventory
      *
-     * @return the name of Inventory
+     * @return the name
      */
-    public InvType getItemName() {
+    public String getName() {
         return invName;
+    }
+
+    /**
+     * Get the name and usage of Inventory
+     *
+     * @return the name and usage
+     */
+    public String getDes() {
+        return switch (this.invName) {
+            case "smallHealth" -> "Small Healing Potion: Add "+ this.amount+ " HP";
+            case "bigHealth" -> "Big Healing Potion: Add "+ this.amount+ " HP";
+            case "smallStamina" -> "Small Stamina Booster: Add "+ this.amount+ " Stamina";
+            case "bigStamina" -> "Big Stamina Booster: Add "+ this.amount+ " Stamina";
+        };
     }
 
 
